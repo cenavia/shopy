c24134325df1db2d30d8d120972fb98aa60f0e32
Carlos Navia <cenaviat@gmail.com>
2016-02-08 18:03:38 -0500

    Implementando Class Based Views product

---
+++ product/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.ProductList.as_view(), name='hello'),
+]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
 
-# Create your views here.
+class ProductList(ListView):
+ 	model = Product
+	
